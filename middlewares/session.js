d2e5c677471e7c983563d49fa83098fc3110edb9
katyampetrova <kkostadinova93@gmail.com>
2022-12-11 16:07:45 +0200

    fix

--- middlewares/session.js
+++ middlewares/session.js
@@ -6,7 +6,7 @@ module.exports = () => (req, res, next) => {
     // console.log(req.headers.cookie.replace('accessToken=', ''));
 
     const token = req.headers.cookie?.replace('accessToken=', '');
-    console.log(req.cookie);
+    // console.log(req.cookie);
     // console.log(token);
     if (token) {
         try {
@@ -17,6 +17,7 @@ module.exports = () => (req, res, next) => {
             return res.status(401).json({ message: 'Invalid authorization token'});
         }
     } else {
+        res.cookie('accessToken', '000000', { httpOnly: true });
         return res.status(401).json({ message: 'Invalid authorization token'});
     }
 
