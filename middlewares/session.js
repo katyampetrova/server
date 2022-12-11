2b8bbb8f1141ea403eb50d329801d0865365ee5e
katyampetrova <kkostadinova93@gmail.com>
2022-12-11 13:01:15 +0200

    log

--- middlewares/session.js
+++ middlewares/session.js
@@ -6,7 +6,7 @@ module.exports = () => (req, res, next) => {
     // console.log(req.headers.cookie.replace('accessToken=', ''));
 
     const token = req.headers.cookie?.replace('accessToken=', '');
-    console.log(rreq.headers.cookie);
+    console.log(req.headers.cookie);
     // console.log(token);
     if (token) {
         try {
